931e5782ea02b417a150fd00eab203dd2660da43
jade0819 <break0819@gmail.com>
2023-09-20 14:18:25 +0900

    Chore: jsx-a11y 플러그인 설치, Link 태그 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+  ],
+  overrides: [
+    {
+      files: ['.eslintrc.{js,cjs}'],
+      env: {
+        jest: true,
+      },
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'no-var': 'error',
+    'no-multiple-empty-lines': 'error',
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    eqeqeq: 'error',
+    'no-unused-vars': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'jsx-a11y/anchor-has-content': [
+      'warn',
+      {
+        components: ['Link'],
+        specialLink: ['to'],
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        components: ['Link'],
+        specialLink: ['to'],
+      },
+    ],
+  },
+};
